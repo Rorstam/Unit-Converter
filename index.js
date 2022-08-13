889d98e73fdeaa572d45557e87754b763feb8285
RorstamFilip <rorstamfilip@gmail.com>
2022-08-13 21:20:04 +0200

    Created a cool unit converter based on a course online!

---
+++ index.js
@@ -0,0 +1,25 @@
+let number = 0
+
+const feet = 3.281
+const gallon = 0.264
+const pound = 2.204
+
+const btnConvert = document.getElementById("btn-convert")
+const txtConvert = document.getElementById("txt-convert")
+
+const pLength = document.getElementById("p-length")
+const pVolume = document.getElementById("p-volume")
+const pMass = document.getElementById("p-mass")
+
+
+btnConvert.addEventListener("click", function() {
+    let number = txtConvert.value
+    
+    let length = `${number} meters = ${(feet * number).toFixed(3)} feet | ${number} feet = ${(number / feet).toFixed(3)} meters`
+    let volume = `${number} meters = ${(gallon * number).toFixed(3)} gallons | ${number} gallons = ${(number / gallon).toFixed(3)} meters`
+    let mass = `${number} meters = ${(pound * number).toFixed(3)} pounds | ${number} pounds = ${(number / pound).toFixed(3)} meters`
+
+    pLength.innerHTML = length
+    pVolume.innerHTML = volume
+    pMass.innerHTML = mass
+})
